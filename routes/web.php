886be701f6bc287e71440ba605a9ee19fee629ed
Shinji Tsukamoto <s.tsukamoto4114@gmail.com>
2020-03-23 22:42:51 +0000

    php_13

--- routes/web.php
+++ routes/web.php
@@ -14,16 +14,19 @@
 Route::get('/', function () {
     return view('welcome');
 });
-Route::group(['prefix' => 'admin'], function(){
-    Route::get('news/create', 'Admin\NewsController@add')->middleware('auth');
+Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function(){
+    Route::get('news/create', 'Admin\NewsController@add');
+    Route::post('news/create','Admin\NewsController@create');
 });
 //php 09 課題　3.
 Route::get('XXX','AAAController@bbb');
 
-//php 09 課題　4.
-Route::group(['prefix' => 'admin'], function(){
-    Route::get('profile/create','Admin\ProfileController@add')->middleware('auth');
-    Route::get('profile/edit','Admin\ProfileController@edit')->middleware('auth');
+//php 09 課題　4. php 13 課題 3,6
+Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function(){
+    Route::get('profile/create','Admin\ProfileController@add');
+    Route::post('profile/create','Admin\ProfileController@create');
+    Route::get('profile/edit','Admin\ProfileController@edit');
+    Route::post('profile/edit','Admin\ProfileController@update');
 });
     
 
