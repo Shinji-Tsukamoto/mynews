d22e9490093cf5fa0d6ff2b69a5489ba0d977362
Shinji Tsukamoto <s.tsukamoto4114@gmail.com>
2020-03-31 11:21:56 +0000

    php_17

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -6,6 +6,8 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
 use App\Profile;
+use App\History2;
+use Carbon\Carbon;
 
 class ProfileController extends Controller
 {
@@ -56,6 +58,11 @@ class ProfileController extends Controller
         $profile_form =$request->all();
         unset($profile_form['_token']);
         $profile->fill($profile_form)->save();
+        
+        $history = new History2;
+        $history->profile_id = $profile->id;
+        $history->edited_at = Carbon::now();
+        $history->save();
         return redirect('admin/profile');
     }
     
